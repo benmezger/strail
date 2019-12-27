43abb1fe96e42e2c75ee993a4f770e34fc5dc034
Ben Mezger <me@benmezger.nl>
2019-12-26 22:42:07 -0300

    Feat: Add disable_irq macro which disables all interrupts

--- include/arch/riscv/cpu.h
+++ include/arch/riscv/cpu.h
@@ -5,6 +5,8 @@
  * https://github.com/sifive/freedom-metal/blob/master/src/privilege.c
  * 2c96ceb44c9a6a489846aaaebf7bdc2dc710b80c
  */
+#define MSTATUS_MIE_OFFSET 3
+#define MSTATUS_MPIE_OFFSET 7
 
 #define clear_csr(reg, bit)                                                 \
 	({                                                                  \
@@ -32,6 +34,12 @@
 				     : "=r"(__tmp)                          \
 				     : "r"(bit));                           \
 		__tmp;                                                      \
+	})
+
+#define disable_irq()                                   \
+	({                                              \
+		clear_csr(mie, MSTATUS_MEIP_OFFSET);    \
+		clear_csr(mstatus, MSTATUS_MIE_OFFSET); \
 	})
 
 #endif
