7dc639e0dfceeff316329458f909af64a27431d6
Ben Mezger <me@benmezger.nl>
2020-01-23 22:36:22 -0300

    Feat: print unsigned int as hexadecimal and print pointer's addresses
    The new formats are:
    
    - %x - prints the hex value of the given int
    - %p - prints the given address of the pointer
    
    The public printk public calls are:
    
    * printk(const char *fmt, ...)
    * _printk(const char)
    * _printk_hex(uintptr_t)
    * iter_left_to_right(unsigned int, void (*f)(const char))

--- include/kernel/printk.h
+++ include/kernel/printk.h
@@ -1,10 +1,12 @@
 #ifndef _PRINTK_H
 #define _PRINTK_H
 
+#include <stdint.h>
 #include <stdarg.h>
 
 void _printk(const char);
 void printk(const char *fmt, ...);
 void iter_left_to_right(unsigned int, void (*f)(const char));
+void _printk_hex(uintptr_t);
 
 #endif
--- src/printk.c
+++ src/printk.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <kernel/printk.h>
 #include <drivers/uart.h>
 
@@ -25,13 +26,37 @@ iter_left_to_right(unsigned int n, void (*callback)(const char))
 	}
 }
 
+char hex_digit(int);
+char
+hex_digit(int v)
+{
+	if (v >= 0 && v < 10) {
+		return '0' + v;
+	}
+	return 'a' + v - 10;
+}
+
+void
+_printk_hex(uintptr_t ptr)
+{
+
+	_printk('0');
+	_printk('x');
+
+	int i;
+	for (i = (sizeof(ptr) << 3) - 4; i >= 0; i -= 4) {
+		_printk(hex_digit((ptr >> i) & 0xF));
+	}
+}
+
 void
 printk(const char *fmt, ...)
 {
 	va_list argp;
 	va_start(argp, fmt);
 
-	int value;
+	int value, i;
+	uintptr_t ptr;
 	char c;
 
 	while (*fmt != '\0') {
@@ -45,6 +70,16 @@ printk(const char *fmt, ...)
 				continue;
 			case 'c':
 				_printk(va_arg(argp, int));
+				fmt++;
+				continue;
+			case 'x':
+				ptr = va_arg(argp, uintptr_t);
+				_printk_hex(ptr);
+				fmt++;
+				continue;
+			case 'p':
+				ptr = va_arg(argp, uintptr_t);
+				_printk_hex(ptr);
 				fmt++;
 				continue;
 			default:
